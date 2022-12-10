235f991db5feac8a51db7e012e5d31489eb089d3
brucy34 <brucygio123@gmail.com>
2022-12-10 14:16:44 -0500

    transform some def function to lambda function and other corrections

--- mastering_function.py
+++ mastering_function.py
@@ -6,8 +6,7 @@ from slugify import slugify
 # 3.-kod  aleyatwa alfabetik san repetisyon
 alfa1="abcdefghijklmnopqrstuvwxyz"
 
-def aleyatwa1(alfa,nomb):
-    return "".join(random.sample(alfa,nomb))
+aleyatwa1 = lambda alfa,nomb : "".join(random.sample(alfa,nomb))
 
 
 kod1=aleyatwa1(alfa1,10)
@@ -19,8 +18,7 @@ print(len(kod1))
 
 alfa2="abcdefghijklmnopqrstuvwxyz1234567890"
 
-def aleyatwa2(alfa,nomb):
-    return "".join(random.sample(alfa,nomb))
+aleyatwa2=lambda alfa,nomb: "".join(random.sample(alfa,nomb))
 
 
 kod2=aleyatwa2(alfa2,36)
@@ -31,16 +29,14 @@ print(len(kod2))
 
 alfa3="yon --c'henn pou fé\\slug"
 
-def slug(alfa):
-    return slugify(alfa)
+slug= lambda alfa: slugify(alfa)
 
 print(slug(alfa3))
 
 # 6.- separe chak let pa yon vigil
 mesaj1="metevigigilyoaprelet"
 
-def met_vigil(mesaj):
-    return ",".join([elt for elt in mesaj])
+met_vigil=lambda mesaj: ",".join([elt for elt in mesaj])
 
 print(met_vigil(mesaj1))
 
@@ -48,8 +44,7 @@ print(met_vigil(mesaj1))
 
 mesaj2= "ALO"
 
-def crypt(mesaj: str):
-    return "-".join([str(ord(elt)-97) for elt in mesaj.lower()])
+crypt=lambda mesaj: "-".join([str(ord(elt)-97) for elt in mesaj.lower()])
 
 print(crypt(mesaj2))
 
@@ -57,8 +52,7 @@ print(crypt(mesaj2))
 
 mesaj3="0-11-14"
 
-def decrypt(mesaj:str):
-    return "".join(chr(int(elt)+97) for elt in re.findall(r'\d+',mesaj))
+decrypt=lambda mesaj: "".join(chr(int(elt)+97) for elt in re.findall(r'\d+',mesaj))
 
 print(decrypt(mesaj3))
 
@@ -78,7 +72,6 @@ permute(pemite1,pemite2)
 
 nom="JEAN-BAPTISTE JEAN"
 
-def inisyal(nom:str):
-    return "".join([n[0] for n in nom.replace("-"," ").split()])
+inisyal=lambda nom: "".join([n[0] for n in nom.replace("-"," ").split()])
 
 print(inisyal(nom)) 
